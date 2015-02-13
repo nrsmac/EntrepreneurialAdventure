cf304d71b3afb3210ae34a59a330b403082a7c48
Noah Schill <noahrschill@gmail.com>
2015-02-13 10:10:25 -0600

    Added testVariable

--- app/src/main/java/com/nrsmac/entrepreneurialadventure/MainActivity.java
+++ app/src/main/java/com/nrsmac/entrepreneurialadventure/MainActivity.java
@@ -6,10 +6,14 @@ import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
 
+    int testVariable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        testVariable = 4;
+
     }
 
 
