bee60308360bcb22dc9c7856e0bcdf09595e95e0
Noah Schill <noahrschill@gmail.com>
2015-02-13 09:30:46 -0600

    Initial Commit

---
+++ app/src/main/java/com/nrsmac/entrepreneurialadventure/storyActivity.java
@@ -0,0 +1,14 @@
+package com.nrsmac.entrepreneurialadventure;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class storyActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
